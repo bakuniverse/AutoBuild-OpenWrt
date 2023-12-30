bf8d7a7841911c84dd7932abe7dd1b8aa84b3170
bakuniverse <30513244+bakuniverse@users.noreply.github.com>
2023-12-30 17:18:52 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -13,3 +13,26 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
+#----------------------------------------------------------------------------#
+# If On Github Actions, remove bloat to get space (~ 30 GB)
+if [ "$USER" = "runner" ] || [ "$(whoami)" = "runner" ]; then
+   echo -e "\n[+] Debloating GH Runner...\n"
+     #12.0 GB
+     sudo rm /usr/local/lib/android -rf 2>/dev/null
+     #8.2 GB
+     sudo rm /opt/hostedtoolcache/CodeQL -rf 2>/dev/null
+     #5.0 GB
+     sudo rm /usr/local/.ghcup -rf 2>/dev/null
+     #2.0 GB
+     sudo rm /usr/share/dotnet -rf 2>/dev/null
+     #1.7 GB
+     sudo rm /usr/share/swift -rf 2>/dev/null
+     #1.1 GB
+     #sudo rm /usr/local/lib/node_modules -rf 2>/dev/null
+     #1.0 GB
+     sudo rm /usr/local/share/powershell -rf 2>/dev/null
+     #500 MB
+     sudo rm /usr/local/lib/heroku -rf 2>/dev/null
+fi
+#----------------------------------------------------------------------------#
+
