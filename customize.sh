617572cab7d19e7e93afbfe18b37dfb395b4f342
bakuniverse <30513244+bakuniverse@users.noreply.github.com>
2024-11-21 17:01:18 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -16,8 +16,15 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' /home/runner/work/AutoBuild-Ope
 #rm openwrt/package/lean/luci-theme-argon -rf
 rm openwrt/feeds/luci/applications/luci-app-passwall2 -rf
 rm openwrt/feeds/luci/applications/luci-app-passwall -rf
+rm -rf openwrt/feeds/packages/net/xray-core
+rm -rf openwrt/feeds/packages/net/chinadns-ng
+rm -rf openwrt/feeds/packages/net/sing-box
+rm -rf openwrt/feeds/packages/net/dns2socks
+rm -rf openwrt/feeds/packages/net/dns2tcp
+rm -rf openwrt/feeds/packages/net/tcping
 cp -a /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/passwall/luci-app-passwall /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/luci/applications
 cp -a /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/passwall/luci-app-passwall /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/luci/applications
+
 #----------------------------------------------------------------------------#
  #If On Github Actions, remove bloat to get space (~ 30 GB)
 if [ "$USER" = "runner" ] || [ "$(whoami)" = "runner" ]; then
