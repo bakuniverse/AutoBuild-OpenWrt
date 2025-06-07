74c9921776b983bc484dd9665c090a04c35d4898
bakuniverse <30513244+bakuniverse@users.noreply.github.com>
2025-06-07 10:50:05 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -16,19 +16,28 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' /home/runner/work/AutoBuild-Ope
 #rm openwrt/package/lean/luci-theme-argon -rf
 rm openwrt/feeds/luci/applications/luci-app-passwall2 -rf
 rm openwrt/feeds/luci/applications/luci-app-passwall -rf
-#rm -rf openwrt/feeds/packages/net/xray-core
-#rm -rf openwrt/feeds/packages/net/chinadns-ng
-#rm -rf openwrt/feeds/packages/net/sing-box
-#rm -rf openwrt/feeds/packages/net/dns2socks
-#rm -rf openwrt/feeds/packages/net/dns2tcp
+rm -rf openwrt/package/lean/passwall
+rm -rf openwrt/package/lean/passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git openwrt/package/lean/passwall_package
+git clone https://github.com/xiaorouji/openwrt-passwall.git openwrt/package/lean/passwall
+git clone https://github.com/xiaorouji/openwrt-passwall2.git openwrt/package/lean/passwall2
+#cp -rf /home/demo/lede/package/lean/passwall_package/* openwrt/package/lean/passwall
+rm -rf openwrt/feeds/packages/net/geoview
+rm -rf openwrt/feeds/packages/net/microsocks
+rm -rf openwrt/feeds/packages/net/xray-core
+rm -rf openwrt/feeds/packages/net/chinadns-ng
+rm -rf openwrt/feeds/packages/net/sing-box
+rm -rf openwrt/feeds/packages/net/dns2socks
+rm -rf openwrt/feeds/packages/net/dns2tcp
 #rm -rf openwrt/feeds/packages/net/tcping
-cp -a /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/passwall/luci-app-passwall /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/luci/applications
-cp -a /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/passwall2/luci-app-passwall2 /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/luci/applications
-cp -a /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/passwall_packages/xray-core /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/packages/net
-cp -a /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/passwall_packages/chinadns-ng /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/packages/net
-cp -a /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/passwall_packages/sing-box /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/packages/net
-cp -a /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/passwall_packages/dns2socks /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/packages/net
-cp -a /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/passwall_packages/tcping /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/packages/net
+#cp -a /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/passwall/luci-app-passwall /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/luci/applications
+#cp -a /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/passwall2/luci-app-passwall2 /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/luci/applications
+#cp -a /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/passwall_packages/xray-core /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/packages/net
+#cp -a /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/passwall_packages/chinadns-ng /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/packages/net
+#cp -a /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/passwall_packages/sing-box /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/packages/net
+#cp -a /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/passwall_packages/dns2socks /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/packages/net
+#cp -a /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/passwall_packages/tcping /home/runner/work/AutoBuild-OpenWrt/AutoBuild-OpenWrt/openwrt/feeds/packages/net
+sed -i 's/local RUN_NEW_DNSMASQ=1/local RUN_NEW_DNSMASQ=0/g' openwrt/package/lean/passwall/luci-app-passwall/root/usr/share/passwall/app.sh
 
 #----------------------------------------------------------------------------#
  #If On Github Actions, remove bloat to get space (~ 30 GB)
